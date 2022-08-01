254d416c6cc6a055e333dbd1ef2afb52c3294ed7
Thomas He <godcantpee@gmail.com>
2022-08-01 10:18:36 -0700

    init project

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -4,6 +4,8 @@ import { Platform, StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+// tab is locatated at information button on top right
+
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
